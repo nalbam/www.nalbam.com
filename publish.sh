97528c2ae89a60e182218f7aa28cfa3f7cbe9e1a
nalbam <me@nalbam.com>
2019-08-03 20:13:00 +0900

    publish

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+RUN_PATH=$(dirname $0)
+
+BUCKET="${CIRCLE_PROJECT_REPONAME}"
+
+# aws s3 sync
+_command "aws s3 sync ${RUN_PATH}/src/main/webapp/ s3://${PUBLISH_PATH}/ --acl public-read"
+aws s3 sync ${RUN_PATH}/src/main/webapp/ s3://${PUBLISH_PATH}/ --acl public-read
+
+# aws cf reset
+CFID=$(aws cloudfront list-distributions --query "DistributionList.Items[].{Id:Id,Origin:Origins.Items[0].DomainName}[?contains(Origin,'${BUCKET}')] | [0]" | grep 'Id' | cut -d'"' -f4)
+if [ "${CFID}" != "" ]; then
+    aws cloudfront create-invalidation --distribution-id ${CFID} --paths "/*"
+fi
